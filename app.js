e09d1a26d4fa8e5c36f4cd60ff002991ed8af53a
Ezra Tsai <ishow8166@icloud.com>
2020-08-07 12:07:17 +0800

    feat: add boostrap/fontawesome stylesheet and body-parser

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars')
 const Shortener = require('./models/shortener')
 const shortener = require('./models/shortener')
@@ -19,11 +20,18 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname:'.hbs'}))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
     Shortener.find()
     .lean()
     .then(shorteners => res.render('index', { shorteners}))
     .catch(error => console.log(error))    
+})
+
+app.post('/', (req,res) => {
+    console.log('req.body', req.body)
+    res.render('index')
 })
 
 app.listen(PORT, () => {
